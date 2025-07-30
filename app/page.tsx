324767c8fd9f61700b28ab32a2634992e8d14f0a
Tasnia-H <tasniahossain2014@gmail.com>
2025-07-30 17:48:33 +0600

    test2

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
     return (
       <div className="min-h-screen flex items-center justify-center bg-gray-50">
         <div className="flex flex-col items-center space-y-4">
-          <p>Yoooooo</p>
           {/* Company Logo */}
           <div className="relative">
             <img
