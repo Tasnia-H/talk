7bf5b8262b3223acf6ede1d0ff9f97c4cf4586c8
Tasnia-H <tasniahossain2014@gmail.com>
2025-07-30 11:05:30 +0600

    test

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -39,6 +39,7 @@ export default function AuthForm() {
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
+          <h1>Talk</h1>
           <h2 className="mt-6 text-center text-2xl sm:text-3xl font-extrabold text-gray-900">
             {isLogin ? "Sign in to your account" : "Create new account"}
           </h2>
