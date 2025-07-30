2aa29fe86cfe3a6c4529ed355cd3eba69f31123b
Tasnia-H <tasniahossain2014@gmail.com>
2025-07-30 17:09:35 +0600

    Fixed urls

--- components/ChatInterface.tsx
+++ components/ChatInterface.tsx
@@ -156,7 +156,7 @@ export default function ChatInterface() {
     if (!token) return;
 
     console.log("Initializing socket connection");
-    const socketInstance = io("http://localhost:3001", {
+    const socketInstance = io("https://tbk.solar-ict.com", {
       auth: { token },
       forceNew: false,
       reconnection: true,
@@ -358,7 +358,7 @@ export default function ChatInterface() {
 
   const fetchUsers = async () => {
     try {
-      const response = await fetch("http://localhost:3001/users", {
+      const response = await fetch("https://tbk.solar-ict.com/users", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
